e7ac3b1cb0cc7ccc77e41c6591feff908b2b852f
kaclok <572005238@qq.com>
2025-02-07 08:55:19 +0800

    多模块

--- backend/launcher/src/main/java/com/smlj/launcher/LauncherApplication.java
+++ backend/launcher/src/main/java/com/smlj/launcher/LauncherApplication.java
@@ -1,20 +1,15 @@
 package com.smlj.launcher;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Slf4j
 @EnableCaching
 @EnableAsync
 @EnableWebMvc
-@EnableAspectJAutoProxy(exposeProxy = true) // http://www.voidcn.com/article/p-zddcuyii-bpt.html
 // 有了 多数据源 和动态台数据源 需要屏蔽默认的数据源配置形式
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class/*, SecurityAutoConfiguration.class*/})
 /*@MapperScan({"com.smlj.train.logic.o.vo.table.dao", "com.smlj.train.logic.o.vo.table.dao.common"})*/
